ccdc952edf63e3bdd3679e574fb2ce173b5ecb02
Francis Granger <franki_le_pro@hotmail.com>
2018-11-01 00:59:02 -0400

    dumb scroller to view

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom'
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import styles from './App.css';
+import Button from '@material-ui/core/Button';
 
 class App extends Component {
+  rowsContent = { Experience: null, Education: null, Projects: null, Skills: null, Awards: null };
+
+  scrollToDomRef = (val) => {
+    const myDomNode = ReactDOM.findDOMNode(this.rowsContent[val])
+    window.scrollTo(0, myDomNode.offsetTop - 64)
+  }
+
   render() {
-    const numberOfRows = Array.from(Array(10).keys());
-    const rows = numberOfRows.map((index) =>
-      <Grid container className={styles.rowContent}>
+    const rows = Object.keys(this.rowsContent).map((value, index) =>
+      <Grid key={value} container className={styles.rowContent} ref={r => { this.rowsContent[value] = r }}>
         <Grid item xs style={{ backgroundColor: index % 2 === 0 ? "#303030" : "#F0F0F0" }}>
           haut gauche
         </Grid>
@@ -25,6 +33,9 @@ class App extends Component {
             <Typography variant="title" >
               Myself
             </Typography>
+            {Object.keys(this.rowsContent).map((value) =>
+              <Button onClick={() => this.scrollToDomRef(value)}>{value}</Button>
+            )}
           </Toolbar>
         </AppBar>
         <div className={styles.appLayout}>
